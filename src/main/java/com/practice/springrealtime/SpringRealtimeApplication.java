1f781600c15b57f78cf9708f6e4fa34182d6e4b0
mynuni <elelaka0000@gmail.com>
2024-04-18 23:53:41 +0900

    Initial commit

---
+++ src/main/java/com/practice/springrealtime/SpringRealtimeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springrealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRealtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRealtimeApplication.class, args);
+    }
+
+}
